be956dadb015a470f6b275ce86e4634ad96c7847
Łukasz Sudół <wukashisudol@gmail.com>
2025-08-20 11:46:44 +0200

    get message from json with pain001 in base64 nested

--- src/main/java/org/accenture/payment/resource/MessageResource.java
+++ src/main/java/org/accenture/payment/resource/MessageResource.java
@@ -4,6 +4,7 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.accenture.payment.service.MessageServiceJsonBase64;
 import org.jboss.resteasy.reactive.server.multipart.MultipartFormDataInput;
 import org.accenture.payment.service.MessageServiceMultipart;
 
@@ -24,7 +25,24 @@ public class MessageResource {
             return Response.ok(resp).build();
         } catch (Exception e) {
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                    .entity("Multipart error: " + e.toString()).build();
+                    .entity("Multipart error: " + e).build();
+        }
+    }
+
+    @POST
+    @Path("/jsonbase64")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_XML)
+    public Response processServiceJsonBase64(String jsonBody) {
+        try {
+            String xmlResponse = MessageServiceJsonBase64.processJsonBase64(jsonBody);
+            return Response.ok(xmlResponse, MediaType.APPLICATION_XML).build();
+        } catch (Exception e) {
+            String errorXml = "<error>\nInvalid JSON or Base64 data:\n " + e + "\n</error>";
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity(errorXml)
+                    .type(MediaType.APPLICATION_XML)
+                    .build();
         }
     }
 }
